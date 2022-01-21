47f5a198ddd778136531b29713a15f61f5e389c3
Apollo Wayne <waynezhaoyang@gmail.com>
2022-01-21 11:12:37 +0800

    Update SNMP Helper

--- SnmpHelper.cs
+++ SnmpHelper.cs
@@ -10,29 +10,66 @@ using Lextm.SharpSnmpLib;
 
 namespace LanMonitor
 {
+    public class SwitchDevice
+    {
+        public class SwitchPort
+        {
+            public string Name { get; set; }
+            public string Brief { get; set; }
+            public bool IsUp { get; set; }
+        }
+        public string IPAddress { get; set; }
+        public List<SwitchPort> PortList { get; set; }
+    }
+    public class SwitchHost
+    {
+        public string HostName { get; set; }
+        public string IPAddress { get; set; }
+        public string MACAddress { get; set; }
+    }
     internal class SnmpHelper
     {
+        public static class OIDString
+        {
+            public const string OID_ARP = "";
+        }
+
+        public static string AuthUsername;
+        public static string AuthPassword;
+        public static string PrivPassword;
+        public static List<string> SwitchIPList;
+        public const int Timeout = 60000;
+        public const int RetryCount = 10;
+
+        public void Initialize()
+        {
+            SwitchIPList = new List<string>()
+            {
+                "172.16.24.1"
+            };
+
+        }
+
         [Obsolete]
         public static void GetData()
         {
             Discovery discovery = Messenger.GetNextDiscovery(SnmpType.GetBulkRequestPdu);
-            ReportMessage report = discovery.GetResponse(60000, new IPEndPoint(IPAddress.Parse("172.16.24.1"), 161));
+            ReportMessage report = discovery.GetResponse(Timeout, new IPEndPoint(IPAddress.Parse("172.16.24.1"), 161));
 
-            var auth = new SHA1AuthenticationProvider(new OctetString("admin@huawei"));
-            var priv = new AESPrivacyProvider(new OctetString("admin@huawei"), auth);
-            var result = new List<Variable>();
-            var count = Messenger.BulkWalk(VersionCode.V3,
+            SHA1AuthenticationProvider auth = new SHA1AuthenticationProvider(new OctetString("admin@huawei"));
+            AESPrivacyProvider priv = new AESPrivacyProvider(new OctetString("admin@huawei"), auth);
+            List<Variable> result = new List<Variable>();
+            var unused = Messenger.BulkWalk(VersionCode.V3,
                               new IPEndPoint(IPAddress.Parse("172.16.24.1"), 161),
                               new OctetString("admin"),
                               OctetString.Empty,
                               new ObjectIdentifier("1.3.6.1.2.1.4.22.1.2"),
                               result,
-                              60000,
+                              Timeout,
                               10,
                               WalkMode.WithinSubtree,
                               priv,
                               report);
-            var a = result;
         }
     }
 }
