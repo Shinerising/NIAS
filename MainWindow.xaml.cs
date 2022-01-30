99392c19326022d5f59ef879c6b763b85846b65c
Apollo Wayne <shinoring@msn.cn>
2022-01-30 12:06:41 +0800

    Add Mouse Title Event Handler

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace LanMonitor
 {
@@ -60,6 +63,70 @@ namespace LanMonitor
 
         private void Window_Closing(object sender, CancelEventArgs e)
         {
+        }
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            var source = PresentationSource.FromVisual(this);
+            ((HwndSource)source)?.AddHook(Hook);
+        }
+
+        const int WM_MOUSEHWHEEL = 0x020E;
+
+        private IntPtr Hook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_MOUSEHWHEEL:
+                    int tilt = (short)HIWORD(wParam);
+                    OnMouseTilt(tilt);
+                    return (IntPtr)1;
+            }
+            return IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// Gets high bits values of the pointer.
+        /// </summary>
+        private static int HIWORD(IntPtr ptr)
+        {
+            int val32 = (int)ptr.ToInt64();
+            return ((val32 >> 16) & 0xFFFF);
+        }
+
+        /// <summary>
+        /// Gets low bits values of the pointer.
+        /// </summary>
+        private static int LOWORD(IntPtr ptr)
+        {
+            int val32 = (int)ptr.ToInt64();
+            return (val32 & 0xFFFF);
+        }
+
+        private void OnMouseTilt(int tilt)
+        {
+            UIElement element = Mouse.DirectlyOver as UIElement;
+
+            if (element == null) return;
+
+            ScrollViewer scrollViewer = element is ScrollViewer viewer ? viewer : FindParent<ScrollViewer>(element);
+
+            if (scrollViewer == null)
+                return;
+
+            scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset + tilt * 0.25);
+        }
+
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null) return null;
+
+            T parent = parentObject as T;
+            if (parent != null)
+                return parent;
+            else
+                return FindParent<T>(parentObject);
         }
 
         public void Dispose()
