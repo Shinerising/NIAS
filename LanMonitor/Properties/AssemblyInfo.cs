f7edb8846f0b3c2a0affd991f2aeb90eebd5f806
Apollo Wayne <shinoring@msn.cn>
2022-09-04 18:40:52 +0800

    Update Version Files

--- LanMonitor/Properties/AssemblyInfo.cs
+++ LanMonitor/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("网络智能分析系统")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NIAS 网络智能分析系统")]
+[assembly: AssemblyDescription("一款能够即时显示局域网内设备连接和数据通信状态的监控工具")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NIAS")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyCompany("CRSCD")]
+[assembly: AssemblyProduct("NIAS 网络智能分析系统")]
+[assembly: AssemblyCopyright("© 2022 Apollo Wayne")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.4.7")]
-[assembly: AssemblyFileVersion("0.2.4.7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
