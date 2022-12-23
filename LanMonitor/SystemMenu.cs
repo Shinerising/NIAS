91318e9bc86a5b89edd46f131ad400da7f460bf4
Apollo Wayne <shinoring@msn.cn>
2022-12-23 16:16:14 +0800

    Update System Menu

--- LanMonitor/SystemMenu.cs
+++ LanMonitor/SystemMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -25,13 +26,18 @@ namespace LanMonitor
         private const uint MF_ENABLED = 0x0;
 
         private const uint MF_DISABLED = 0x2;
+
         private const uint COMMAND_HELP = 101;
         private const uint COMMAND_ABOUT = 102;
+        private const uint COMMAND_OPTION = 103;
 
         [DllImport("user32.dll")]
-        static extern int GetMenuItemCount(IntPtr hMenu);
+        private static extern int GetMenuItemCount(IntPtr hMenu);
         [DllImport("user32.dll")]
         private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CreateMenu();
 
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         private static extern bool InsertMenu(IntPtr hmenu, int position, uint flags, uint item_id, [MarshalAs(UnmanagedType.LPWStr)] string item_text);
@@ -41,12 +47,13 @@ namespace LanMonitor
 
         public static void ApplyCustomMenuItems(IntPtr handle)
         {
+            IntPtr ShortcutMenu = CreateMenu();
             IntPtr systemMenu = GetSystemMenu(handle, false);
             int count = GetMenuItemCount(systemMenu);
             InsertMenu(systemMenu, count - 1, MF_BYPOSITION | MF_SEPARATOR, 0, string.Empty);
-            InsertMenu(systemMenu, count - 1, MF_BYPOSITION | MF_STRING | MF_DISABLED, COMMAND_ABOUT, AppResource.GetString("Menu_Option"));
-            InsertMenu(systemMenu, count - 0, MF_BYPOSITION | MF_STRING, COMMAND_HELP, AppResource.GetString("Menu_Help"));
-            InsertMenu(systemMenu, count + 1, MF_BYPOSITION | MF_STRING, COMMAND_ABOUT, AppResource.GetString("Menu_About"));
+            InsertMenu(systemMenu, count - 1, MF_BYPOSITION | MF_STRING, COMMAND_OPTION, string.Format("{0}{1}{2}", AppResource.GetString("Menu_Option"), ControlChars.Tab, "Alt + O"));
+            InsertMenu(systemMenu, count - 0, MF_BYPOSITION | MF_STRING, COMMAND_HELP, string.Format("{0}{1}{2}", AppResource.GetString("Menu_Help"), ControlChars.Tab, "Alt + H"));
+            InsertMenu(systemMenu, count + 1, MF_BYPOSITION | MF_STRING, COMMAND_ABOUT, string.Format("{0}{1}{2}", AppResource.GetString("Menu_About"), ControlChars.Tab, "Alt + A"));
         }
 
         public static string HandleMenuCommand(int menuID)
