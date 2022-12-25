7e8c3044f2fb86a9e5ed9addc52a93527daf7b59
Apollo Wayne <waynezhaoyang@gmail.com>
2022-12-25 11:45:44 +0800

    Update Dependencies

--- ReportTemplate/vite.config.ts
+++ ReportTemplate/vite.config.ts
@@ -1,15 +1,27 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { resolve, dirname } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
+  plugins: [
+    vue(),
+    viteSingleFile(),
+    VueI18nPlugin({
+      /* options */
+      // locale messages resource pre-compile option
+      include: resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "./src/locales/**"
+      ),
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
+  }
 });
