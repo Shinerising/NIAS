f15185c80144aad782641824d96b8db574669509
Apollo Wayne <shinoring@msn.cn>
2022-12-27 11:54:11 +0800

    Update Template Generator

--- ReportGenerator/ReportGenerator.cs
+++ ReportGenerator/ReportGenerator.cs
@@ -2,25 +2,17 @@
 {
     public class ReportGenerator
     {
-        private readonly string templatePath;
         private const string scriptTag = "<script id=\"rawData\" type=\"application/json\">{0}</script>";
-        public ReportGenerator(string templatePath)
-        {
-            this.templatePath = templatePath;
-        }
 
-        public void ApplyData(string data, string target)
+        public static void ApplyData(string data, string template, string target)
         {
             string tempFile = Path.GetTempFileName();
-            File.Copy(templatePath, tempFile, true);
+            File.Copy(template, tempFile, true);
             using StreamWriter sw = File.AppendText(tempFile);
             sw.WriteLine(string.Format(scriptTag, data));
+            sw.Flush();
+            sw.Dispose();
             File.Move(tempFile, target);
-        }
-
-        public void ExportReport(string directory, string filename)
-        {
-
         }
     }
 }
--- ReportGenerator/ReportManager.cs
+++ ReportGenerator/ReportManager.cs
@@ -1,20 +1,37 @@
-﻿namespace NIASReport
+﻿using System.IO;
+using System.Text.Json;
+
+namespace NIASReport
 {
     public class ReportManager
     {
         public string ReportDirectory { get; private set; }
         public string ReportTemplatePath { get; private set; }
-        public string StationName { get; private set; }
-        public ReportManager(string directory, string template, string stationName, int triggerTime)
+        public string LocationName { get; private set; }
+        public ReportManager(string directory, string template, string location, int triggerTime)
         {
             ReportDirectory = directory;
             ReportTemplatePath = template;
-            StationName = stationName;
+            LocationName = location;
         }
 
         public void ListReport()
         {
 
+        }
+
+        public void GenerateNewReport()
+        {
+            ReportData data = new()
+            {
+                Title = "测试数据",
+                Location = LocationName,
+                User = "测试人员",
+                CreateTime = DateTime.Now
+            };
+            string json = JsonSerializer.Serialize(data, new JsonSerializerOptions() { });
+            string filename = Path.Combine(ReportDirectory, string.Format("NetworkReport {0} {1}.html", LocationName, DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")));
+            ReportGenerator.ApplyData(json, ReportTemplatePath, filename);
         }
     }
 }
