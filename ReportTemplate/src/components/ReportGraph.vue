d0ce82fb56f4f72691744768a53e97755d45d139
Apollo Wayne <waynezhaoyang@gmail.com>
2023-01-05 18:01:32 +0800

    Update Graph

--- ReportTemplate/src/components/ReportGraph.vue
+++ ReportTemplate/src/components/ReportGraph.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import moment from "moment";
 import { ref, provide } from "vue";
 import { use } from "echarts/core";
 import { SVGRenderer } from "echarts/renderers";
@@ -29,7 +30,6 @@ import IconRouter from "./icons/IconRouter.vue";
 import IconHub from "./icons/IconHub.vue";
 
 import { ImageComputer, ImageSwitch } from "./images/ImageResource";
-import type { EChartsOption } from "echarts/types/dist/shared";
 
 defineProps<{
   data: ReportData;
@@ -100,162 +100,127 @@ const option01 = (() => {
 })();
 
 const option02 = (() => {
-  const data = [
-    ["2000-06-05", 116],
-    ["2000-06-06", 129],
-    ["2000-06-07", 135],
-    ["2000-06-08", 86],
-    ["2000-06-09", 73],
-    ["2000-06-10", 85],
-    ["2000-06-11", 73],
-    ["2000-06-12", 68],
-    ["2000-06-13", 92],
-    ["2000-06-14", 130],
-    ["2000-06-15", 245],
-    ["2000-06-16", 139],
-    ["2000-06-17", 115],
-    ["2000-06-18", 111],
-    ["2000-06-19", 309],
-    ["2000-06-20", 206],
-    ["2000-06-21", 137],
-    ["2000-06-22", 128],
-    ["2000-06-23", 85],
-    ["2000-06-24", 94],
-    ["2000-06-25", 71],
-    ["2000-06-26", 106],
-    ["2000-06-27", 84],
-    ["2000-06-28", 93],
-    ["2000-06-29", 85],
-    ["2000-06-30", 73],
-    ["2000-07-01", 83],
-    ["2000-07-02", 125],
-    ["2000-07-03", 107],
-    ["2000-07-04", 82],
-    ["2000-07-05", 44],
-    ["2000-07-06", 72],
-    ["2000-07-07", 106],
-    ["2000-07-08", 107],
-    ["2000-07-09", 66],
-    ["2000-07-10", 91],
-    ["2000-07-11", 92],
-    ["2000-07-12", 113],
-    ["2000-07-13", 107],
-    ["2000-07-14", 131],
-    ["2000-07-15", 111],
-    ["2000-07-16", 64],
-    ["2000-07-17", 69],
-    ["2000-07-18", 88],
-    ["2000-07-19", 77],
-    ["2000-07-20", 83],
-    ["2000-07-21", 111],
-    ["2000-07-22", 57],
-    ["2000-07-23", 55],
-    ["2000-07-24", 60],
-  ];
-
-  const dateList = data.map(function (item) {
-    return item[0];
-  });
-  const valueList = data.map(function (item) {
-    return item[1];
-  });
+  const startDate = new Date().getTime();
+  const cpu = Array.from({ length: 24 * 60 }, (x, i) => [
+    new Date(startDate + 60000 * i),
+    Math.random() * 0.6 + 0.1,
+  ]);
+  const memory = Array.from({ length: 24 * 60 }, (x, i) => [
+    new Date(startDate + 60000 * i),
+    Math.random() * 0.3 + 0.2,
+  ]);
+  const temperature = Array.from({ length: 24 * 60 }, (x, i) => [
+    new Date(startDate + 60000 * i),
+    Math.random() * 40 + 20,
+  ]);
 
   return ref({
-    visualMap: [
-      {
-        show: false,
-        type: "continuous",
-        seriesIndex: 0,
-        min: 0,
-        max: 400,
-      },
-      {
-        show: false,
-        type: "continuous",
-        seriesIndex: 1,
-        dimension: 0,
-        min: 0,
-        max: dateList.length - 1,
-      },
-    ],
-    title: [
-      {
-        left: "center",
-        text: "Gradient along the y axis",
-      },
-      {
-        top: "55%",
-        left: "center",
-        text: "Gradient along the x axis",
-      },
-    ],
+    title: {
+      text: "设备工作状态曲线",
+    },
     tooltip: {
       trigger: "axis",
+      axisPointer: {
+        axis: "x",
+      },
     },
-    xAxis: [
-      {
-        data: dateList,
+    legend: {
+      show: true,
+    },
+    xAxis: {
+      type: "time",
+      axisLabel: {
+        formatter: (value: Date) => moment(value).format("MM-DD HH:mm"),
       },
-      {
-        data: dateList,
-        gridIndex: 1,
-      },
-    ],
+    },
     yAxis: [
-      {},
       {
-        gridIndex: 1,
+        name: "CPU&内存占用率",
+        type: "value",
+        max: 1,
+        min: 0,
+        axisLabel: {
+          formatter: (value: number) => (value * 100).toFixed(0) + "%",
+        },
       },
-    ],
-    grid: [
       {
-        bottom: "60%",
+        name: "",
+        type: "value",
+        alignTicks: true,
+        position: "left",
+        max: 1,
+        min: 0,
+        show: false,
       },
       {
-        top: "60%",
+        name: "设备温度",
+        type: "value",
+        alignTicks: true,
+        max: 120,
+        min: 20,
+        axisLabel: {
+          formatter: (value: number) => value.toFixed(0) + "℃",
+        },
       },
     ],
     series: [
       {
         type: "line",
+        name: "CPU占用率",
+        data: cpu,
+        large: true,
+        animation: false,
         showSymbol: false,
-        data: valueList,
       },
       {
         type: "line",
-        showSymbol: false,
-        data: valueList,
-        xAxisIndex: 1,
+        name: "内存占用率",
         yAxisIndex: 1,
+        data: memory,
+        large: true,
+        animation: false,
+        showSymbol: false,
+      },
+      {
+        type: "line",
+        name: "设备温度",
+        yAxisIndex: 2,
+        data: temperature,
+        large: true,
+        animation: false,
+        showSymbol: false,
       },
     ],
   });
 })();
 
 const option03 = (() => {
-  // prettier-ignore
-  const hours = [
-    '12a', '1a', '2a', '3a', '4a', '5a', '6a',
-    '7a', '8a', '9a', '10a', '11a',
-    '12p', '1p', '2p', '3p', '4p', '5p',
-    '6p', '7p', '8p', '9p', '10p', '11p'
-  ];
+  const startDate = new Date().getTime();
 
-  // prettier-ignore
-  const days = [
-    'Saturday', 'Friday', 'Thursday',
-    'Wednesday', 'Tuesday', 'Monday', 'Sunday'
-  ];
-
-  // prettier-ignore
-  const data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 6, 0], [0, 7, 0], [0, 8, 0], [0, 9, 0], [0, 10, 0], [0, 11, 2], [0, 12, 4], [0, 13, 1], [0, 14, 1], [0, 15, 3], [0, 16, 4], [0, 17, 6], [0, 18, 4], [0, 19, 4], [0, 20, 3], [0, 21, 3], [0, 22, 2], [0, 23, 5], [1, 0, 7], [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0], [1, 5, 0], [1, 6, 0], [1, 7, 0], [1, 8, 0], [1, 9, 0], [1, 10, 5], [1, 11, 2], [1, 12, 2], [1, 13, 6], [1, 14, 9], [1, 15, 11], [1, 16, 6], [1, 17, 7], [1, 18, 8], [1, 19, 12], [1, 20, 5], [1, 21, 5], [1, 22, 7], [1, 23, 2], [2, 0, 1], [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 5, 0], [2, 6, 0], [2, 7, 0], [2, 8, 0], [2, 9, 0], [2, 10, 3], [2, 11, 2], [2, 12, 1], [2, 13, 9], [2, 14, 8], [2, 15, 10], [2, 16, 6], [2, 17, 5], [2, 18, 5], [2, 19, 5], [2, 20, 7], [2, 21, 4], [2, 22, 2], [2, 23, 4], [3, 0, 7], [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 1], [3, 9, 0], [3, 10, 5], [3, 11, 4], [3, 12, 7], [3, 13, 14], [3, 14, 13], [3, 15, 12], [3, 16, 9], [3, 17, 5], [3, 18, 5], [3, 19, 10], [3, 20, 6], [3, 21, 4], [3, 22, 4], [3, 23, 1], [4, 0, 1], [4, 1, 3], [4, 2, 0], [4, 3, 0], [4, 4, 0], [4, 5, 1], [4, 6, 0], [4, 7, 0], [4, 8, 0], [4, 9, 2], [4, 10, 4], [4, 11, 4], [4, 12, 2], [4, 13, 4], [4, 14, 4], [4, 15, 14], [4, 16, 12], [4, 17, 1], [4, 18, 8], [4, 19, 5], [4, 20, 3], [4, 21, 7], [4, 22, 3], [4, 23, 0], [5, 0, 2], [5, 1, 1], [5, 2, 0], [5, 3, 3], [5, 4, 0], [5, 5, 0], [5, 6, 0], [5, 7, 0], [5, 8, 2], [5, 9, 0], [5, 10, 4], [5, 11, 1], [5, 12, 5], [5, 13, 10], [5, 14, 5], [5, 15, 7], [5, 16, 11], [5, 17, 6], [5, 18, 0], [5, 19, 5], [5, 20, 3], [5, 21, 4], [5, 22, 2], [5, 23, 0], [6, 0, 1], [6, 1, 0], [6, 2, 0], [6, 3, 0], [6, 4, 0], [6, 5, 0], [6, 6, 0], [6, 7, 0], [6, 8, 0], [6, 9, 0], [6, 10, 1], [6, 11, 0], [6, 12, 2], [6, 13, 1], [6, 14, 3], [6, 15, 4], [6, 16, 0], [6, 17, 0], [6, 18, 0], [6, 19, 0], [6, 20, 1], [6, 21, 2], [6, 22, 2], [6, 23, 6]]
-    .map(function (item) {
-      return [item[1], item[0], item[2] || '-'];
-    });
+  const data = Array.prototype.concat(
+    ...Array.from({ length: 24 }, (x, i) =>
+      Array.from({ length: 24 * 60 }, (_x, _i) => [
+        startDate + 60000 * _i,
+        i,
+        Math.random() * 1024 || "-",
+      ])
+    )
+  );
 
   const option = {
+    title: {
+      text: "网口流量变化",
+    },
     tooltip: {
       position: "top",
+      formatter: (args: unknown) => {
+        const { data } = args as {
+          data: [x: string, y: number, value: number];
+        };
+        return `时间：${moment(data[0]).format("MM-DD HH:mm:ss")}<br>网口：${
+          data[1]
+        }<br>流量：${data[2].toFixed(2)}mbps`;
+      },
     },
     grid: {
       height: "50%",
@@ -263,40 +228,38 @@ const option03 = (() => {
     },
     xAxis: {
       type: "category",
-      data: hours,
       splitArea: {
         show: true,
+      },
+      axisLabel: {
+        formatter: (value: string) =>
+          moment(parseInt(value)).format("MM-DD HH:mm"),
       },
     },
     yAxis: {
       type: "category",
-      data: days,
-      splitArea: {
-        show: true,
-      },
     },
     visualMap: {
       min: 0,
-      max: 10,
-      calculable: true,
+      max: 1024,
       orient: "horizontal",
       left: "center",
       bottom: "15%",
+      calculable: true,
+      realtime: false,
+      inRange: {
+        color: ["white", "lightgreen", "orange", "red"],
+      },
     },
     series: [
       {
-        name: "Punch Card",
         type: "heatmap",
         data: data,
         label: {
-          show: true,
+          show: false,
         },
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowColor: "rgba(0, 0, 0, 0.5)",
-          },
-        },
+        progressive: 10240000,
+        animation: false,
       },
     ],
   };
@@ -308,16 +271,17 @@ const [option04, option06] = (() => {
   const generateData = () => {
     const data = [];
     for (let i = 0; i < 24 * 60; i++) {
+      const l = Math.random() * 20;
       data.push([
         new Date(startDate.getTime() + i * 60000),
-        Math.floor(Math.random() * 4),
+        l > 18 ? 3 : l > 16 ? 2 : l > 2 ? 1 : 0,
       ]);
     }
     return data;
   };
   const option01 = {
     title: {
-      text: "Graph",
+      text: "网络拓扑图",
     },
     series: [
       {
@@ -438,7 +402,10 @@ const [option04, option06] = (() => {
     ],
   };
 
-  const option02: EChartsOption = {
+  const option02 = {
+    title: {
+      text: "网络状态图",
+    },
     tooltip: {
       trigger: "axis",
       axisPointer: {
@@ -447,30 +414,42 @@ const [option04, option06] = (() => {
     },
     xAxis: {
       type: "time",
+      axisLabel: {
+        formatter: (value: Date) => moment(value).format("MM-DD HH:mm"),
+      },
     },
     yAxis: {
       type: "category",
+      splitLine: {
+        show: false,
+      },
+      axisTick: {
+        show: false,
+      },
+      axisLine: {
+        show: false,
+      },
       data: [
         {
-          value: "idle",
+          value: "无数据",
           textStyle: {
             color: "gray",
           },
         },
         {
-          value: "normal",
+          value: "正常",
           textStyle: {
             color: "green",
           },
         },
         {
-          value: "warning",
+          value: "警告",
           textStyle: {
             color: "orange",
           },
         },
         {
-          value: "error",
+          value: "故障",
           textStyle: {
             color: "red",
           },
@@ -478,8 +457,7 @@ const [option04, option06] = (() => {
       ],
     },
     visualMap: {
-      top: 50,
-      right: 10,
+      show: false,
       pieces: [
         {
           min: -1,
@@ -514,8 +492,10 @@ const [option04, option06] = (() => {
       {
         data: generateData(),
         type: "scatter",
-        symbolSize: [1, 16],
+        animation: false,
+        symbolSize: [1, 32],
         symbol: "rect",
+        large: true,
       },
     ],
   };
@@ -541,7 +521,6 @@ const option05 = (() => {
       data: ["bar", "bar2"],
     },
     toolbox: {
-      // y: 'bottom',
       feature: {
         magicType: {
           type: ["stack"],
