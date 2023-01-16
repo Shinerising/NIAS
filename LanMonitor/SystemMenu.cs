7e27a6e65c002826ae172a32b0af9f058fdc09d8
Apollo Wayne <shinoring@msn.cn>
2023-01-16 11:49:50 +0800

    Update System Menu

--- LanMonitor/SystemMenu.cs
+++ LanMonitor/SystemMenu.cs
@@ -9,45 +9,28 @@ using System.Windows.Interop;
 
 namespace LanMonitor
 {
-    internal static class SystemMenu
+    internal static partial class SystemMenu
     {
-        private const uint WM_SYSCOMMAND = 0x112;
-
-        private const uint WM_INITMENUPOPUP = 0x0117;
 
         private const uint MF_SEPARATOR = 0x800;
-
-        private const uint MF_BYCOMMAND = 0x0;
-
         private const uint MF_BYPOSITION = 0x400;
-
         private const uint MF_STRING = 0x0;
-
-        private const uint MF_ENABLED = 0x0;
-
-        private const uint MF_DISABLED = 0x2;
 
         private const uint COMMAND_HELP = 101;
         private const uint COMMAND_ABOUT = 102;
         private const uint COMMAND_OPTION = 103;
 
-        [DllImport("user32.dll")]
-        private static extern int GetMenuItemCount(IntPtr hMenu);
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [LibraryImport("user32.dll")]
+        private static partial int GetMenuItemCount(IntPtr hMenu);
+        [LibraryImport("user32.dll")]
+        private static partial IntPtr GetSystemMenu(IntPtr hWnd, [MarshalAs(UnmanagedType.Bool)] bool bRevert);
 
-        [DllImport("user32.dll")]
-        private static extern IntPtr CreateMenu();
-
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern bool InsertMenu(IntPtr hmenu, int position, uint flags, uint item_id, [MarshalAs(UnmanagedType.LPWStr)] string item_text);
-
-        [DllImport("user32.dll")]
-        private static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+        [LibraryImport("user32.dll", EntryPoint = "InsertMenuW", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool InsertMenu(IntPtr hmenu, int position, uint flags, uint item_id, [MarshalAs(UnmanagedType.LPWStr)] string item_text);
 
         public static void ApplyCustomMenuItems(IntPtr handle)
         {
-            IntPtr ShortcutMenu = CreateMenu();
             IntPtr systemMenu = GetSystemMenu(handle, false);
             int count = GetMenuItemCount(systemMenu);
             InsertMenu(systemMenu, count - 1, MF_BYPOSITION | MF_SEPARATOR, 0, string.Empty);
