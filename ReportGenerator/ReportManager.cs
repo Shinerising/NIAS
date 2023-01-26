1a5b6ea2c53afa15823c13d3a18f92f2ada6dae9
Apollo Wayne <shinoring@msn.cn>
2023-01-26 22:17:14 +0800

    Add File List

--- ReportGenerator/ReportManager.cs
+++ ReportGenerator/ReportManager.cs
@@ -2,13 +2,29 @@
 {
     public class ReportManager
     {
+        public class ReportFileInfo
+        {
+            public string Name { get; set; }
+            public string Directory { get; set; }
+            public DateTime CreateTime { get; set; }
+            public ReportFileInfo(FileInfo info)
+            {
+                Name = info.Name;
+                Directory = info.Directory?.Name ?? "";
+                CreateTime = info.CreationTime;
+            }
+        }
         public event ErrorEventHandler? ErrorHandler;
 
+        private const int ExpiredDays = 32;
+        private readonly string FileDirectory;
         private readonly ReportRecorder recorder;
         private readonly ReportGenerator generator;
         private readonly DatabaseHelper dbHelper;
         public ReportManager(string directory, string template, string location, int triggerTime)
         {
+            FileDirectory = directory;
+
             dbHelper = DatabaseHelper.Initialize("raw.sqlite");
             dbHelper.ErrorHandler += HandleError;
 
@@ -60,6 +76,28 @@
         public void GenerateReport()
         {
             generator.IsGenerateRequested = true;
+        }
+        public List<ReportFileInfo> GetFileList()
+        {
+            return Directory.GetFiles(FileDirectory, "NetworkReport*.html").Select(item => new ReportFileInfo(new FileInfo(item))).ToList();
+        }
+        public void DeleteExpiredFiles()
+        {
+            try
+            {
+                foreach (var file in Directory.GetFiles(FileDirectory, "NetworkReport*.html"))
+                {
+                    var info = new FileInfo(file);
+                    if (DateTime.Now - info.CreationTime > TimeSpan.FromDays(ExpiredDays))
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                HandleError(this, new ErrorEventArgs(e));
+            }
         }
     }
 }
