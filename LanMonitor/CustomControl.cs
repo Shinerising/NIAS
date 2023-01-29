c150bafa652d4a2a3fb62f961c58193681d8091a
Apollo Wayne <shinoring@msn.cn>
2023-01-29 16:00:59 +0800

    Add Mask

--- LanMonitor/CustomControl.cs
+++ LanMonitor/CustomControl.cs
@@ -54,6 +54,19 @@ namespace LanMonitor
         }
     }
 
+    public class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class TimeValueConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
